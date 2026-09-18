9bce92188de3589b6d10f11fc71aa08fb3eef708
dhirajhack6 <159863667+dhirajhack6@users.noreply.github.com>
2026-09-18 18:16:48 +0530

    second commit

---
+++ backend/src/app.js
@@ -0,0 +1,18 @@
+import express from " express";
+import { createServer } from "node:http";
+
+import { Server } from "socket.io";
+
+import mongoose from "mongoose";
+
+import cors from "cors";
+
+const app = express();
+
+app.get("/home", (req, res) => {
+    return res.json({ "hello": "worlds" })
+});
+
+const start = async () => {
+    
+}
